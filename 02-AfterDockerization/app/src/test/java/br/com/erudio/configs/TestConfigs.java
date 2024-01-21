75d5192662fd2935521f54335e90f3701e474d5d
leandrocgsi <leandrocgsi@gmail.com>
2024-01-20 21:56:25 -0300

    Continuous Integration with Github, Github Actions and Docker Hub

--- 02-AfterDockerization/app/src/test/java/br/com/erudio/configs/TestConfigs.java
+++ 02-AfterDockerization/app/src/test/java/br/com/erudio/configs/TestConfigs.java
@@ -2,7 +2,7 @@ package br.com.erudio.configs;
 
 public class TestConfigs {
 
-	public static final int SERVER_PORT = 8888;
+	public static final int SERVER_PORT = 80;
 	
 	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
 	public static final String HEADER_PARAM_ORIGIN = "Origin";
